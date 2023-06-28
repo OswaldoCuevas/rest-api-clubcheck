308fdd96f7762e68bcccbd66c22c573ab3e340e1
Tu nombre de usuario <oscuevas18@gmail.com>
2023-06-27 18:47:25 -0700

    add function outbound_sync

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -13,5 +13,41 @@ export const addListUsers = async (req, res) => {
     res.send("sincronización entrante") 
   }
   export const OutBoundSync = async (req, res) => {
-    res.send("sincronización saliente") 
+    const user_id = "id_user1";
+    // {
+    //   usuarios: objeto de usuario
+    //   suscripciones: [objeto de suscripción],
+    //   cliente : objeto cliente,
+    //   asistencias : objeto de asistencia,
+    // }
+   
+     try{
+      const [users] = await pool.query('SELECT * FROM users where id = ?;', [user_id]);
+      const [user] = users;
+      try{
+        const [attendances] = await pool.query('SELECT * FROM attendances where customer = ?;', [user.customer]);
+        const [attendance] = attendances;
+        try{
+          const [subscriptions] = await pool.query('SELECT * FROM subscriptions where user = ?;', [users.id]);
+          try{
+            const [customers] = await pool.query('SELECT * FROM customers where id = ?;', [user.customer]);
+            const [customer] = customers;
+              res.json({user,customer,subscriptions,attendance});
+           }catch(e){
+            //const codeError = e.code;
+            res.status(500).json({e});
+           }
+         }catch(e){
+         // const e = e.code;
+          res.status(500).json({e});
+         }
+       }catch(e){
+       // const e = e.code;
+        res.status(500).json({e});
+       }
+     }catch(e){
+     // const e = e.code;
+     console.log(e+"sa");
+      res.status(500).json({e});
+     }
   }
